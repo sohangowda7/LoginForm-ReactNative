bb754745f18d3dfc8ef10a75b7dc92b5a9766896
sohangowda7 <sohan.n2002@gmail.com>
2024-07-09 14:54:13 +0530

    color code updated

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const styles=StyleSheet.create({
   backgroundColor: "white"
  },
  form  : { 
- backgroundColor : "white" ,
+ backgroundColor : "#015F83" ,
   padding:20 , 
   borderRadius:10,
   shadowColor:"black",
